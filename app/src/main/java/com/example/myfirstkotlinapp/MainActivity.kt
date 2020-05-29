0e46fbff306f451971e6b30287b7f67bdc34a18b
ThanushkaDulanja <thanushkadb@gmail.com>
2020-05-29 09:37:51 +0530

    MAD Kotlin Tutorial - improve interface

--- app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -6,14 +6,6 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
-//class MainActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//}
-
 
 class MainActivity : AppCompatActivity() {
 
